bc9c7ca48c94ad653bd31ac4f8fa3f10718a7770
Christopher Garvis <cgarvis@gmail.com>
2013-01-20 00:39:17 -0500

    Moves configuration into before all blocks

--- spec/integration/memory_spec.rb
+++ spec/integration/memory_spec.rb
@@ -3,12 +3,12 @@ require 'perpetuity/memory'
 require 'perpetuity_shared'
 
 describe Perpetuity::Memory do
-  context do
+  before(:all) do
     Perpetuity.configure { data_source Perpetuity::Memory.new }
-
-    it_behaves_like "mappable"
-    it_behaves_like "persistable"
-    it_behaves_like "crud"
-    it_behaves_like "pagination"
   end
+
+  it_behaves_like "mappable"
+  it_behaves_like "persistable"
+  it_behaves_like "crud"
+  it_behaves_like "pagination"
 end
--- spec/integration/mongodb_spec.rb
+++ spec/integration/mongodb_spec.rb
@@ -3,16 +3,16 @@ require "perpetuity/mongodb"
 require 'perpetuity_shared'
 
 describe Perpetuity::MongoDB do
-  context do
-    Perpetuity.configure { Perpetuity::MongoDB.new db: 'perpetuity_gem_test' }
-
-    it_behaves_like "mappable"
-    it_behaves_like "persistable"
-    it_behaves_like "crud"
-    it_behaves_like "pagination"
-    it_behaves_like "associations"
-    it_behaves_like "validation"
-    it_behaves_like "scopable"
-    it_behaves_like "indexable"
+  before(:all) do
+    Perpetuity.configure { data_source Perpetuity::MongoDB.new db: 'perpetuity_gem_test' }
   end
+
+  it_behaves_like "mappable"
+  it_behaves_like "persistable"
+  it_behaves_like "crud"
+  it_behaves_like "pagination"
+  it_behaves_like "associations"
+  it_behaves_like "validation"
+  it_behaves_like "scopable"
+  it_behaves_like "indexable"
 end
